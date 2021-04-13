f5123097c0b84ec9711f1484903691eda15de5c9
Sheba467 <lowyouone@gmail.com>
2021-04-14 01:06:20 +0300

    fix style

--- app.js
+++ app.js
@@ -1,120 +1,105 @@
-const prices = { small: 2.5, medium: 4.66, large: 9 }
+const prices = {
+    small: 2.5,
+    medium: 4.66,
+    large: 9
+}
 let data = {
-	firstName: '',
-	lastName: '',
-	email: '',
-	totalAmount: 0,
-	// inputAmount: 0,
-	quantities: {
-		small: 0,
-		medium: 0,
-		large: 0
-	},
+    firstName: '',
+    lastName: '',
+    email: '',
+    totalAmount: 0,
+    quantities: {
+        small: 0,
+        medium: 0,
+        large: 0
+    },
 }
 
-function add(itemType) {	
-	data.quantities[itemType]++;
-	data.totalAmount += prices[itemType];
-	updateQuantity(itemType);
-	updateBalance();
+function add(itemType) {
+    data.quantities[itemType]++;
+    data.totalAmount += prices[itemType];
+    updateQuantity(itemType);
+    updateBalance();
 }
 
-function remove(itemType ) {
-	if (data.quantities[itemType] === 0) {
-		alert('you do not have any packages');
-		return;
-	}
-
-	data.totalAmount -= prices[itemType];
-	data.quantities[itemType]--;
-	updateQuantity(itemType);
-	updateBalance();
+function remove(itemType) {
+    if (data.quantities[itemType] === 0) {
+        alert('You do not have any packages');
+        return;
+    }
+    data.totalAmount -= prices[itemType];
+    data.quantities[itemType]--;
+    updateQuantity(itemType);
+    updateBalance();
 }
 
 function updateQuantity(itemType) {
-	const quantityInput = document.querySelector(`.quantityInput#${itemType}`)
-	quantityInput.innerText = data.quantities[itemType]
-	
-	// const quantityInputNumber = document.querySelector(`.quantityInputNumber#${itemType}`)
-	// quantityInputNumber.innerText = data.inputAmount[itemType]
-	
+    const quantityInput = document.querySelector(`.quantityInput#${itemType}`)
+    quantityInput.innerText = data.quantities[itemType]
+}
 
+function updateCheckout(countElem, rowElem, quantity) {
+    const countSpan = document.getElementById(countElem)
+    const itemRow = document.getElementById(rowElem)
+    countSpan.innerHTML = quantity
+    itemRow.setAttribute('data-count', quantity)
 }
 
 function updateBalance() {
-	const totalAmountSMall = document.getElementById('totalAmountSmall');
-	const totalSmall = data.quantities.small * prices.small;
-	// totalAmountSMall.innerText = totalSmall;
 
-	const totalAmountMedium = document.getElementById('totalAmountMedium');
-	const totalMedium = data.quantities.medium * prices.medium;
-	// totalAmountMedium.innerText = totalMedium;
+    const totalSmall = data.quantities.small * prices.small;
+	updateCheckout('quantityInputNumberSmall', 'bottlePackSmallSummary', data.quantities.small)
+	
+    const totalMedium = data.quantities.medium * prices.medium;
+	updateCheckout('quantityInputNumberMedium', 'bottlePackMediumSummary', data.quantities.medium)
+	
+    const totalLarge = data.quantities.large * prices.large;
+    updateCheckout('quantityInputNumberLarge', 'bottlePackLargeSummary', data.quantities.large)
 
-	const totalAmountLarge = document.getElementById('totalAmountLarge');
-	const totalLarge = data.quantities.large * prices.large;
-	// totalAmountLarge.innerText = totalLarge;
+    const totalAmount = document.getElementById('totalAmount');
+    totalAmount.innerText = `$${parseFloat(totalSmall + totalMedium + totalLarge).toFixed(2)}`
 
-	const totalAmount = document.getElementById('totalAmount');
-	totalAmount.innerText = `$${parseFloat(totalSmall + totalMedium + totalLarge).toFixed(2)}`
+    save();
+
 }
 
 function save() {
-	localStorage.setItem('data', JSON.stringify(data));
+    localStorage.setItem('data', JSON.stringify(data));
 }
 
 function load() {
-	const rawData = localStorage.getItem('data');
-	if (!rawData) {
-		return;
-	}
-	data = JSON.parse(rawData)
+    const rawData = localStorage.getItem('data');
+    if (!rawData) {
+        return;
+    }
+    data = JSON.parse(rawData)
+    updateQuantity('small')
+    updateQuantity('medium')
+    updateQuantity('large')
+    updateBalance();
+}
+
+function onOverlay() {
+	const firstnameOverlay = document.getElementById('firstnameOverlay');
+	firstnameOverlay.innerHTML = data.firstName.value
+	document.getElementById("overlay").style.display = "block";
+	
+	const lastnameOverlay = document.getElementById('lastnameOverlay');
+	lastnameOverlay.innerHTML = data.lastName.value
+	document.getElementById("overlay").style.display = "block";
+
+	// const numberOfBottleOverlay = document.getElementById('numberOfBottleOverlay');
+	// numberOfBottleOverlay.innerHTML = data.quantities
+	// document.getElementById("overlay").style.display = "block";
+
+	// const orderPackgesOverlay = document.getElementById('orderPackgesOverlay');
+	// orderPackgesOverlay.innerHTML = data.quantities
+	// document.getElementById("overlay").style.display = "block";
 
 }
 
-// function updateAmount(that) {
-// 	var number = document.getElementById('number');
-// 	var num = parseInt(number.innerHTML);
-// 	num = (that.value == "minus") ? --num : ++num;
-// 	number.innerHTML = num;
-// }
+function off() {
+	document.getElementById("overlay").style.display = "none";
+  }
 
-// function displayPattern(pattern) {
-//     const $li = document.createElement('li')
-//     $li.innerHTML = pattern.content
-//     $li.setAttribute('id', pattern.id)
-//     const deleteButton - document.createElement('button')
-//     deleteButton.textContent = 'Remove this pattern from saves'
-//     deleteButton.setAttribute('class', 'button-style')
-//     $li.append(deleteButton)
-//     patternList.append($li)
-//     deleteButton.addEventListener('click', removePattern)
-// }
-
-
-// function createCard() {
-//     const center = document.createElement('div')
-//     center.classList.add('bottleBackgroundCenter')
-
-//     const bestPriceBackground = document.createElement('div')
-//     bestPriceBackground.classList.add('bestPriceBackground')
-//     center.appendChild(bestPriceBackground)
-
-//     const bestPriceBanner = document.createElement('div')
-//     bestPriceBanner.classList.add('bestPriceBanner')
-//     bestPriceBackground.appendChild(bestPriceBanner)
-
-
-//     const bottlePositionCenter = document.createElement('div')
-//     bottlePositionCenter.classList.add('bottlePositionCenter')
-//     center.appendChild(bottlePositionCenter)
-
-//     return center
-
-// }
-
-// function cardHandler() {
-//     const cardContainer = document.querySelector('.cardContainer')
-//     cardContainer.appendChild(createCard())
-//     cardContainer.appendChild(createCard())
-//     cardContainer.appendChild(createCard())
-// }
+load();
