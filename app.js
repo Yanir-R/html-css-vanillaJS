a3c0cd23da53b86497db569480e978da6e37ef63
Sheba467 <lowyouone@gmail.com>
2021-04-27 14:27:44 +0300

    fixing popup model style

--- app.js
+++ app.js
@@ -48,11 +48,11 @@ function updateCheckout(countElem, rowElem, quantity) {
 function updateBalance() {
 
     const totalSmall = data.quantities.small * prices.small;
-	updateCheckout('quantityInputNumberSmall', 'bottlePackSmallSummary', data.quantities.small)
-	
+    updateCheckout('quantityInputNumberSmall', 'bottlePackSmallSummary', data.quantities.small)
+
     const totalMedium = data.quantities.medium * prices.medium;
-	updateCheckout('quantityInputNumberMedium', 'bottlePackMediumSummary', data.quantities.medium)
-	
+    updateCheckout('quantityInputNumberMedium', 'bottlePackMediumSummary', data.quantities.medium)
+
     const totalLarge = data.quantities.large * prices.large;
     updateCheckout('quantityInputNumberLarge', 'bottlePackLargeSummary', data.quantities.large)
 
@@ -72,23 +72,23 @@ function load() {
     let formClass = document.querySelector('form');
 
     function submitForm(event) {
-            let pack1 = document.querySelector('#small').innerText 
-            let pack2 = document.querySelector('#medium').innerText
-            let pack3 = document.querySelector('#large').innerText
-        
-            let firstName = document.getElementById('firstName').value;
-            document.getElementById('firstnameOverlay').innerHTML = firstName;
-            document.getElementById("overlay").style.display = "block";
-            
-            let lastName = document.getElementById('lastName').value;
-            document.getElementById('lastnameOverlay').innerHTML = lastName;
-            document.getElementById("overlay").style.display = "block";
-            
-            document.querySelector('#numberOfSmallBottleOverlay').innerHTML = 'Number Of Small Packages:' + pack1
-            document.querySelector('#numberOfMediumBottleOverlay').innerHTML = 'Number Of Large Packages:' + pack2
-            document.querySelector('#numberOfLargeBottleOverlay').innerHTML = 'Number Of Large Packages:' + pack3
-            
-            event.preventDefault();
+        let pack1 = document.querySelector('#small').innerText
+        let pack2 = document.querySelector('#medium').innerText
+        let pack3 = document.querySelector('#large').innerText
+
+        let firstName = document.getElementById('firstName').value;
+        document.getElementById('firstnameOverlay').innerHTML = firstName;
+        document.getElementById("overlay").style.display = "block";
+
+        let lastName = document.getElementById('lastName').value;
+        document.getElementById('lastnameOverlay').innerHTML = lastName + ',' + ' you ordered:';
+        document.getElementById("overlay").style.display = "block";
+
+        document.querySelector('#numberOfSmallBottleOverlay').innerHTML = pack1 + ' packs of 6 Bottles pack'
+        document.querySelector('#numberOfMediumBottleOverlay').innerHTML = pack2 + ' packs of 12 Bottles pack'
+        document.querySelector('#numberOfLargeBottleOverlay').innerHTML = pack3 + ' packs of 24 Bottles pack'
+
+        event.preventDefault();
     }
 
     formClass.addEventListener('submit', submitForm);
@@ -101,10 +101,10 @@ function load() {
     updateQuantity('medium')
     updateQuantity('large')
     updateBalance();
-}   
+}
 
 function off() {
-	document.getElementById("overlay").style.display = "none";
-  }
+    document.getElementById("overlay").style.display = "none";
+}
 
 load();
